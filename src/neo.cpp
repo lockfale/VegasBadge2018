b5458a83661208ff40c53bedfab365c354095e0d
pandatrax <pandatrax@gmail.com>
2018-08-09 23:51:41 -0700

    Cleaner looking fire

--- src/neo.cpp
+++ src/neo.cpp
@@ -405,29 +405,27 @@ namespace NEO {
 		// Array of temperature readings at each simulation cell
 		static byte heat[NUM_LEDS];
 
-		if (checkTime(5)) {
-			// Step 1.  Cool down every cell a little
-			for( uint8_t i = 0; i < NUM_LEDS; i++) {
-				heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
-			}
-
-			// Step 2.  Heat from each cell drifts 'up' and diffuses a little
-			for( uint8_t k= NUM_LEDS - 3; k > 0; k--) {
-				heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
-			}
-
-			// Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-			if( random8() < SPARKING ) {
-				int y = random8(7);
-				heat[y] = qadd8( heat[y], random8(160,255) );
-			}
-
-			// Step 4.  Map from heat cells to LED colors
-			for( uint8_t j = 0; j < NUM_LEDS; j++) {
-				leds[j] = HeatColor( heat[j]);
-			}
-			FastLED.show();
+		// Step 1.  Cool down every cell a little
+		for( uint8_t i = 0; i < NUM_LEDS; i++) {
+			heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
 		}
+
+		// Step 2.  Heat from each cell drifts 'up' and diffuses a little
+		for( uint8_t k= NUM_LEDS - 3; k > 0; k--) {
+			heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+		}
+
+		// Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+		if( random8() < SPARKING ) {
+			int y = random8(7);
+			heat[y] = qadd8( heat[y], random8(160,255) );
+		}
+
+		// Step 4.  Map from heat cells to LED colors
+		for( uint8_t j = 0; j < NUM_LEDS; j++) {
+			leds[j] = HeatColor( heat[j]);
+		}
+		FastLED.show();
 	}
 
 	void setPattern(uint8_t p) {
